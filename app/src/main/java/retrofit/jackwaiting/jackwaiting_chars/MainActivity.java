77dca8a6d31522fb9e56e9bb2f7db45e187f4ac6
zhanggang <zhanggang@snaillove.com>
2016-06-27 16:25:37 +0800

    First Commit

---
+++ app/src/main/java/retrofit/jackwaiting/jackwaiting_chars/MainActivity.java
@@ -0,0 +1,23 @@
+package retrofit.jackwaiting.jackwaiting_chars;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CharView charView;
+    private float testData[];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+    private void init() {
+        charView = (CharView) findViewById(R.id.mCharView);
+        testData = new float []{7.70f,7.60f,7.50f,7.45f,7.33f,7.32f,7.25f,7.18f,7.10f,7.01f,6.85f,6.75f,6.74f,6.70f,6.65f,6.63f,6.58f,6.54f,6.50f,6.48f,6.43f,6.38f,6.32f,6.28f,6.18f,6.07f,5.97f,4.50f,4.20f,3.99f,3.33f};
+        charView.getLifeTimeData(testData);
+    }
+}
